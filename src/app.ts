ca517f003c9d53c37521ebbdda2325996d1096ab
Michiels Alexis <pro@alexismch.be>
2020-11-01 00:11:13 +0100

    test

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const io = require('socket.io')(server);
 
 const active_rooms = new Map()
 
-server.listen(80);
+server.listen(process.env.PORT || 3000);
 
 // @ts-ignore
 app.get('/', (req, res) => {
