bb428293464b7dabb7390b0e59623fb714fa6d7e
Alexis Michiels <alexis.michiels@student.vinci.be>
2020-12-05 15:39:41 +0100

    fix: register/connect return type instead of complete user

--- src/modules/connectable.ts
+++ src/modules/connectable.ts
@@ -18,21 +18,21 @@ const expIn = '24h';
  */
 export function connect(req: Request, res: Response, next: NextFunction): any {
 	const body = req.body;
-	if (!body)
-		return next(createError(422, 'body missing'));
+	if (!body) return next(createError(422, 'body missing'));
 	if (!body.email || !EmailValidator.validate(body.email))
-		return next(createError(422, 'field \'email\' missing or invalid'));
+		return next(createError(422, "field 'email' missing or invalid"));
 	if (!body.password)
-		return next(createError(422, 'field \'password\' missing'));
+		return next(createError(422, "field 'password' missing"));
 
-	Connectable
-		.findOne({email: body.email})
+	Connectable.findOne({email: body.email})
 		.then((connectable: IConnectableDoc) => {
 			if (!connectable || !connectable.verifyPassword(body.password))
-				return next(createError(401, 'field \'email\' or \'password\' incorrect'));
+				return next(
+					createError(401, "field 'email' or 'password' incorrect")
+				);
 			res.json({
 				session: generateSessionToken(connectable._id),
-				connectable
+				type: connectable.institution_name ? 'institution' : 'doctor',
 			});
 		})
 		.catch(() => sendError(next));
@@ -46,18 +46,23 @@ export function connect(req: Request, res: Response, next: NextFunction): any {
  * @param connectable the connectable that asked to connect
  * @param paramsErrorMsg the error message to send if error is due to the params
  */
-export function register(req: Request, res: Response, next: NextFunction, connectable: IConnectableDoc, paramsErrorMsg: string): any {
+export function register(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+	connectable: IConnectableDoc,
+	paramsErrorMsg: string
+): any {
 	connectable
 		.save()
 		.then(connectable => {
 			res.status(201).json({
 				session: generateSessionToken(connectable._id),
-				connectable
+				type: connectable.institution_name ? 'institution' : 'doctor',
 			});
 		})
-		.catch((e) => {
-			if (e.code === 11000)
-				return next(createError(409, paramsErrorMsg));
+		.catch(e => {
+			if (e.code === 11000) return next(createError(409, paramsErrorMsg));
 			sendError(next);
 		});
 }
@@ -69,16 +74,24 @@ export function register(req: Request, res: Response, next: NextFunction, connec
  * @param next the next middleware
  * @return response delegated to the next middleware, or with an error
  */
-export function verifySession(req: Request, res: Response, next: NextFunction): void {
+export function verifySession(
+	req: Request,
+	res: Response,
+	next: NextFunction
+): void {
 	if (!req.headers.authorization)
-		return next(createError(401, 'no header \'Authorization\' provided'));
+		return next(createError(401, "no header 'Authorization' provided"));
 
 	const session: string = <string>req.headers.authorization;
 	try {
-		res.locals.session = <string[]><unknown>getSessionConnectableId(session);
+		res.locals.session = <string[]>(
+			(<unknown>getSessionConnectableId(session))
+		);
 		next();
 	} catch (e) {
-		return next(createError(401, 'header \'Authorization\' invalid or expired'));
+		return next(
+			createError(401, "header 'Authorization' invalid or expired")
+		);
 	}
 }
 
@@ -89,7 +102,10 @@ export function verifySession(req: Request, res: Response, next: NextFunction): 
  * @private
  */
 export function generateSessionToken(id: string, exp: string = expIn): string {
-	return sign({
-		id
-	}, exp ? {expiresIn: exp} : {});
+	return sign(
+		{
+			id,
+		},
+		exp ? {expiresIn: exp} : {}
+	);
 }
