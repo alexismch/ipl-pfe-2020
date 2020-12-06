41e0e380ee3acac8e32ac3776d2ae9a15a38630b
Alexis Michiels <alexis.michiels@student.vinci.be>
2020-12-06 23:52:15 +0100

    feat: init front refactor

---
+++ front_end/src/components/Authenticate/Authenticate.tsx
@@ -0,0 +1,69 @@
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import React from 'react';
+import {Link as RouterLink} from 'react-router-dom';
+
+export default function Authenticate({useStyles}) {
+	const classes = useStyles();
+
+	return (
+		<Container component="main" maxWidth="xs">
+			<CssBaseline />
+			<div className={classes.paper}>
+				<Avatar className={classes.avatar}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Authenticate
+				</Typography>
+				<form className={classes.form} noValidate>
+					<TextField
+						variant="outlined"
+						margin="normal"
+						required
+						fullWidth
+						id="email"
+						label="Email Address"
+						name="email"
+						autoComplete="email"
+						autoFocus
+					/>
+					<TextField
+						variant="outlined"
+						margin="normal"
+						required
+						fullWidth
+						name="password"
+						label="Password"
+						type="password"
+						id="password"
+						autoComplete="current-password"
+					/>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="primary"
+						className={classes.submit}
+					>
+						Authenticate
+					</Button>
+					<Grid container justify="center">
+						<Grid item>
+							<Link component={RouterLink} to="/register">
+								Don't have an account yet? Register
+							</Link>
+						</Grid>
+					</Grid>
+				</form>
+			</div>
+		</Container>
+	);
+}
--- front_end/src/components/QRCodeList.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const QRCodeList = () => {
-
-
-    return null;
-}
